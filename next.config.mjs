7fe82af16b00f29d2a5a1b9ef1f74874502d1703
Cyber-boy9 <cyber9boy@gmail.com>
2025-03-27 04:29:20 +0300

    Update next.config.mjs
    Disabled SWC as it is not supported on devices like android ARM64

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  swcMinify: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
